1320d9ff393198bbdf3c260073481d01dfc45e85
shriharieknathe <shriharieknathe999@gmail.com>
2025-08-24 23:24:29 +0530

    Add netflix-clone

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home/Home";
+import Player from "./pages/Player/Player";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  // useEffect(()=>{
+
+  //   onAuthStateChanged(auth, async (user)=>{
+  //     if(user){
+  //       console.log("Logged In");
+  //       navigate('/');
+  //     }else{
+  //       console.log("Logged Out");
+  //       navigate('/login');
+  //     }
+  //   })
+  // },[])
+
+  return (
+    <div>
+      <ToastContainer theme="dark" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path='/login' element={<Login/>}/> */}
+        <Route path="/player/:id" element={<Player />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from "react-router-dom";
+
+const TMDB_Access_Key =  import.meta.env.TMDB_Access_KEY;
+
+const Player = (props) => {
+  const state = props.location;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TMDB_Access_Key}`,
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results[0]))
+      .catch((err) => console.error(err));
+    console.log(props);
+  }, []);
+
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{state}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
